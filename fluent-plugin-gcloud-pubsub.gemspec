b4e67b5e97e352a8ca0e25e6c87136f4394b9c93
Keiji Yoshida <yoshida_keiji@r.recruit.co.jp>
2015-10-09 19:15:14 +0900

    first commit

---
+++ fluent-plugin-gcloud-pubsub.gemspec
@@ -0,0 +1,26 @@
+# encoding: utf-8
+$:.push File.expand_path('../lib', __FILE__)
+
+Gem::Specification.new do |gem|
+  gem.name        = "fluent-plugin-cloud-pubsub"
+  gem.description = "Fluentd plugin for sending data to Cloud Pub/Sub"
+  gem.license     = "MIT"
+  gem.homepage    = "https://github.com/yosssi/fluent-plugin-cloud-pubsub"
+  gem.summary     = gem.description
+  gem.version     = "0.0.1"
+  gem.authors     = ["Keiji Yoshida"]
+  gem.email       = "yoshida.keiji.84@gmail.com"
+  gem.has_rdoc    = false
+  gem.files       = `git ls-files`.split("\n")
+  gem.test_files  = `git ls-files -- {test,spec,features}/*`.split("\n")
+  gem.executables = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  gem.require_paths = ['lib']
+
+  gem.add_runtime_dependency "fluentd"
+  gem.add_runtime_dependency "gcloud", ">= 0.2.0"
+  gem.add_runtime_dependency "yajl-ruby", ">= 1.4.2"
+
+  gem.add_development_dependency "bundler"
+  gem.add_development_dependency "rake"
+  gem.add_development_dependency "test-unit"
+end
---
+++ lib/fluent/plugin/out_cloud_pubsub.rb
@@ -0,0 +1,74 @@
+require 'gcloud'
+
+module Fluent
+  class CloudPubSubOutput < BufferedOutput
+    MAX_REQ_SIZE = 10 * 1024 * 1024 # 10 MB
+    MAX_MSGS_PER_REQ = 1000
+
+    Plugin.register_output('cloud_pubsub', self)
+
+    config_param :project,          :string,  :default => nil
+    config_param :topic,            :string,  :default => nil
+    config_param :key,              :string,  :default => nil
+    config_param :max_req_size,     :integer, :default => MAX_REQ_SIZE
+    config_param :max_msgs_per_req, :integer, :default => MAX_MSGS_PER_REQ
+
+    unless method_defined?(:log)
+      define_method("log") { $log }
+    end
+
+    unless method_defined?(:router)
+      define_method("router") { Fluent::Engine }
+    end
+
+    def configure(conf)
+      super
+
+      raise Fluent::ConfigError, "'project' must be specified." unless @project
+      raise Fluent::ConfigError, "'topic' must be specified." unless @topic
+      raise Fluent::ConfigError, "'key' must be specified." unless @key
+    end
+
+    def start
+      super
+
+      pubsub = (Gcloud.new @project, @key).pubsub
+      @client = pubsub.topic @topic
+    end
+
+    def format(tag, time, record)
+      [tag, time, record].to_msgpack
+    end
+
+    def write(chunk)
+      msgs = []
+      msgs_size = 0
+
+      chunk.msgpack_each do |tag, time, record|
+        size = Yajl.dump(record).bytesize
+        if msgs.length > 0 && (msgs_size + size > max_req_size || msgs.length + 1 > max_msgs_per_req)
+          publish(msgs)
+          msgs = []
+          msgs_size = 0
+        end
+        msgs << record.to_json
+        msgs_size += size
+      end
+
+      if msgs.length > 0
+        publish(msgs)
+      end
+    rescue
+      log.error "unexpected error", :error=>$!.to_s
+      log.error_backtrace
+    end
+  end
+
+  def publish(msgs)
+    @client.publish do |batch|
+      msgs.each do |m|
+        batch.publish m
+      end
+    end
+  end
+end
